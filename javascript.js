f956536ad43b3048ccb031e1ad704dc34dfb890f
ghxusr <nldevsol@protonmail.com>
2024-07-08 19:57:20 +0200

    Add variables for the three parts of the operation and a function that calls the appropiate operation function

--- javascript.js
+++ javascript.js
@@ -13,3 +13,23 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 };
+
+let firstNumber;
+let secondNumber;
+let operator;
+
+function operate(firstNumber, secondNumber, operator) {
+    switch (operator) {
+        case "+":
+            return add(firstNumber, secondNumber);
+            break;
+        case "-":
+            return subtract(firstNumber, secondNumber);
+            break;
+        case "*":
+            return multiply(firstNumber, secondNumber);
+            break;
+        case "/":
+            return divide(firstNumber, secondNumber);
+    };
+};
