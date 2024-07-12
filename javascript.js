5c762f255169529d504669f5500e9b390d44a33f
ghxusr <nldevsol@protonmail.com>
2024-07-12 17:24:17 +0200

    Add function that updates the calculator display when a number button is pressed

--- javascript.js
+++ javascript.js
@@ -33,3 +33,60 @@ function operate(firstNumber, secondNumber, operator) {
             return divide(firstNumber, secondNumber);
     };
 };
+
+let displayValue = "";
+
+const currentDisplay = document.querySelector(".display");
+
+//Select all buttons on calculator
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    //Add event listener to all buttons
+    button.addEventListener("click", () => {
+        //Update current display value when number button is pressed
+        switch(button.id) {
+            case "one-btn":
+                displayValue += "1";
+                currentDisplay.textContent = displayValue;
+                break;
+            case "two-btn":
+                displayValue += "2";
+                currentDisplay.textContent = displayValue;
+                break;
+            case "three-btn":
+                displayValue += "3";
+                currentDisplay.textContent = displayValue;
+                break;
+            case "four-btn":
+                displayValue += "4";
+                currentDisplay.textContent = displayValue;
+                break;
+            case "five-btn":
+                displayValue += "5";
+                currentDisplay.textContent = displayValue;
+                break;
+            case "six-btn":
+                displayValue += "6";
+                currentDisplay.textContent = displayValue;
+                break;
+            case "seven-btn":
+                displayValue += "7";
+                currentDisplay.textContent = displayValue;
+                break;
+            case "eight-btn":
+                displayValue += "8";
+                currentDisplay.textContent = displayValue;
+                break;
+            case "nine-btn":
+                displayValue += "9";
+                currentDisplay.textContent = displayValue;
+                break;
+            case "zero-btn":
+                displayValue += "0";
+                currentDisplay.textContent = displayValue;
+        };
+    });
+});
+
+
