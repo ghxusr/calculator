e89584b7bc2729ea96e05479d4bf532fc054a5a4
ghxusr <nldevsol@protonmail.com>
2024-07-13 17:16:15 +0200

    Add functions to respective operator buttons and expression evaluation

--- javascript.js
+++ javascript.js
@@ -1,22 +1,25 @@
 function add(a, b) {
-    return a + b;
+    return parseInt(a) + parseInt(b);
 };
 
 function subtract(a, b) {
-    return a - b;
+    return parseInt(a) - parseInt(b);
 };
 
 function multiply(a, b) {
-    return a * b;
+    return parseInt(a) * parseInt(b);
 };
 
 function divide(a, b) {
-    return a / b;
+    if (parseInt(b) === 0) {
+        return "You know cannot do that";
+    }
+    else return parseInt(a) / parseInt(b);
 };
 
-let firstNumber;
-let secondNumber;
-let operator;
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
 
 function operate(firstNumber, secondNumber, operator) {
     switch (operator) {
@@ -84,6 +87,58 @@ buttons.forEach((button) => {
                 break;
             case "zero-btn":
                 displayValue += "0";
+                currentDisplay.textContent = displayValue;
+                break;
+            //Call functions for respective buttons pressed
+            case "divide-btn":
+                operator = "/";
+                if (firstNumber === "") {
+                    firstNumber = displayValue;
+                }
+                displayValue = "";
+                break;
+            case "multiply-btn":
+                operator = "*";
+                if (firstNumber === "") {
+                    firstNumber = displayValue;
+                }
+                displayValue = "";
+                break;
+            case "subtract-btn":
+                operator = "-";
+                if (firstNumber === "") {
+                    firstNumber = displayValue;
+                }
+                displayValue = "";
+                break;
+            case "add-btn":
+                operator = "+";
+                if (firstNumber === "") {
+                    firstNumber = displayValue;
+                }
+                displayValue = "";
+                break;
+            case "equal-btn":
+                if (firstNumber != "") {
+                    secondNumber = displayValue;
+                }
+                if (firstNumber != "" && secondNumber != "" && operator != "") {
+                    displayValue = (operate(firstNumber, secondNumber, operator)).toString();
+                    currentDisplay.textContent = displayValue;
+                    firstNumber = "";
+                    secondNumber = "";
+                    operator = "";
+                    //Clear stored value when  dividing by zero
+                    if (displayValue === "You know cannot do that") {
+                    displayValue = "";
+                    }
+                }
+                break;
+            case "clear-btn":
+                firstNumber = "";
+                secondNumber = "";
+                operator = "";
+                displayValue  = "";
                 currentDisplay.textContent = displayValue;
         };
     });
